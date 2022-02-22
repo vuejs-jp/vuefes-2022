e8a22c06aa768364505c5c3c7a72012ec73b03d7
jiyuujin <jiyuujinunite@gmail.com>
2022-02-22 22:33:16 +0900

    add ButtonField for tweets

---
+++ app/components/forms/customize/TweetButtonField.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+interface ITweetButtonFieldEmit {
+  (e: 'onClick'): void
+}
+
+const props = defineProps({
+  titleLabel: {
+    type: String,
+    required: true
+  },
+})
+const emit = defineEmits<ITweetButtonFieldEmit>()
+
+const handleClick = () => {
+  emit('onClick')
+}
+</script>
+
+<template>
+  <button
+    type="submit"
+    class="py-2.5 px-10 w-full text-sm font-medium text-center bg-transparent rounded-lg border focus:ring-4 focus:ring-blue-300 hover:opacity-80 sm:w-auto text-vuefes-tweet border-vuefes-tweet"
+    @click="handleClick"
+  >
+    {{ titleLabel }}
+  </button>
+</template>
