16b4072408b2a448ed08c5f9b53b1e25ab6a18b8
jiyuujin <jiyuujinunite@gmail.com>
2022-02-22 22:34:21 +0900

    add ButtonField for submit

---
+++ app/components/forms/customize/SubmitButtonField.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+interface ISubmitButtonFieldEmit {
+  (e: 'onClick'): void
+}
+
+const props = defineProps({
+  titleLabel: {
+    type: String,
+    required: true
+  },
+})
+const emit = defineEmits<ISubmitButtonFieldEmit>()
+
+const handleClick = () => {
+  emit('onClick')
+}
+</script>
+
+<template>
+  <button
+    type="submit"
+    class="py-2.5 px-10 w-full text-sm font-medium text-center text-white bg-gradient-to-r rounded-lg focus:ring-4 focus:ring-blue-300 hover:opacity-80 sm:w-auto from-vuefes-submit-1 to-vuefes-submit-2"
+    @click="handleClick"
+  >
+    {{ titleLabel }}
+  </button>
+</template>
