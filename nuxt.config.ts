40a122b3e4b6becb87d7f9798aec356adce425b8
jiyuujin <jiyuujinunite@gmail.com>
2022-03-17 21:04:32 +0900

    deploy test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,8 @@ import { defineNuxtConfig } from '@nuxt/bridge'
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
   srcDir: 'app/',
+  target: 'static',
   css: ['~/assets/main.scss'],
   serverMiddleware: [{ path: '/api/hello', handler: '~/server/api/hello.ts' }],
-  buildModules: ['@nuxtjs/svg', '@nuxtjs/tailwindcss']
+  buildModules: ['@nuxtjs/svg', '@nuxtjs/tailwindcss'],
 })
