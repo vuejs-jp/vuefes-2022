9776f94f277b4161280fa2221013822a0dc50805
jiyuujin <jiyuujinunite@gmail.com>
2022-03-25 23:35:40 +0900

    form brushup

--- app/components/FormPageSection.vue
+++ app/components/FormPageSection.vue
@@ -33,7 +33,7 @@ const createSubmit = () => {
   >
     <form>
       <div class="flex flex-col justify-center items-center align-middle">
-        <div class="mb-6 text-center md:px-12 px-90">
+        <div class="3xl:px-0 mb-6 3xl:w-3/5 text-center md:px-36">
           <div class="flex flex-col justify-center items-center">
             <div class="text-6xl opacity-10 font-kokuryu-heading text-vue-blue">{{ `お問い合わせ` }}</div>
             <div class="-m-5 text-4xl font-extrabold lg:w-36 text-vue-blue">{{ `Contact` }}</div>
@@ -43,7 +43,7 @@ const createSubmit = () => {
             v-html="introduction"
           />
         </div>
-        <div class="px-36 mb-6 3xl:w-3/5 md:px-0 md:w-full">
+        <div class="3xl:px-0 mb-6 3xl:w-2/5 md:px-36">
           <InputField
             id-label="name"
             title-label="お名前"
@@ -51,7 +51,7 @@ const createSubmit = () => {
             @on-change="updateName"
           />
         </div>
-        <div class="px-36 mb-6 3xl:w-3/5 md:px-0 md:w-full">
+        <div class="3xl:px-0 mb-6 3xl:w-2/5 md:px-36">
           <InputField
             id-label="email"
             title-label="メールアドレス"
@@ -61,7 +61,7 @@ const createSubmit = () => {
             @on-change="updateEmail"
           />
         </div>
-        <div class="px-36 mb-6 3xl:w-3/5 md:px-0 md:w-full">
+        <div class="3xl:px-0 mb-6 3xl:w-2/5 md:px-36">
           <TextareaField
             id-label="detail"
             title-label="お問い合わせ内容"
