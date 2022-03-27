9f21f67dfe366d725bd76eab5412b3d7bdb82e78
Kohei Tsuchiya <doyahirohira@gmail.com>
2022-03-27 15:35:53 +0900

    Messageセクションのスタイルをデザインに合わせた

--- app/components/MessagePageSection.vue
+++ app/components/MessagePageSection.vue
@@ -1,36 +1,44 @@
+<script setup lang="ts">
+import { representative } from '~/utils/constants'
+</script>
+
 <template>
   <section
     id="message"
-    class="py-40 bg-white"
+    class="py-20 bg-white lg:py-40"
   >
-    <div class="text-center md:px-0 md:w-full md:text-center lg:px-0">
-      <div class="flex justify-center items-center md:flex-row lg:flex-col">
-        <div
-          class="flex flex-col justify-center items-center pb-20 md:pr-12 md:pb-20 md:mr-0 md:text-center lg:mr-0 3xl:pr-0"
-        >
-          <div
-            class="w-60 font-kokuryu-heading text-6xl text-vue-blue opacity-10 lg:w-80 lg:text-center"
-          >
-            {{ `ことづけ` }}
-          </div>
-          <div
-            class="-m-5 w-36 text-4xl font-extrabold text-vue-blue md:w-36 lg:w-36 3xl:text-center"
-          >
-            {{ `Message` }}
-          </div>
+    <div
+      class="px-5 mx-auto max-w-[700px] lg:flex lg:gap-6 lg:justify-between lg:items-center lg:max-w-[1190px]"
+    >
+      <div class="shrink-0 mb-10 text-center lg:mb-20">
+        <div class="font-kokuryu-heading text-[40px] text-vue-blue opacity-10 lg:text-[80px]">
+          ことづけ
         </div>
-        <div class="w-192 text-vue-blue md:w-full lg:pl-24 3xl:px-12">
-          <div
-            class="pb-7 font-normal text-left text-vue-blue"
-            v-html="message"
-          />
-          <div class="font-bold text-right text-vue-blue">{{ representative }}</div>
-        </div>
+        <h2 class="-mt-6 text-xl font-extrabold text-vue-blue lg:-mt-10 lg:text-[40px]">Message</h2>
+      </div>
+      <div class="text-sm leading-7 text-vue-blue lg:max-w-[720px] lg:text-lg lg:leading-8">
+        <p class="mb-6">
+          Vue Fes Japan は 2018 年に誕生した日本最大級の Vue.js カンファレンスです。文字通り Fes =
+          お祭りのように Vue.js
+          を共に盛り上げ、共に学び、そしてなによりも共に楽しむために誕生しました。
+        </p>
+        <p class="mb-6">
+          残念ながら 2019 年は令和元年東日本台風により、2020
+          年以降は新型コロナウイルス感染症により中止となり、痛恨の極みとなりましたが、私たちはカンファレンス開催を諦めず、力を貯めつつ活動を続けてきました。
+        </p>
+        <p class="mb-6">
+          この数年で Vue.js を取り巻く環境は激変しました。Vue.js 3.0 のリリース、そして Vue.js
+          の作者 Evan You 氏によって新たな OSS として Vite.js が作られ、JavaScript
+          コミュニティに放たれました。Vue.js や Nuxt.js といった OSS はもちろん、それ以外に Vitest
+          といった Vue.js 以外の新しい OSS エコシステムが形成しつつあります。
+        </p>
+        <p class="mb-6">
+          そうした状況で盛り上がる中、この度 2022 年、オンラインカンファレンスとして Vue Fes Japan
+          の開催を決定しましたので、ここにお知らせいたします。
+        </p>
+        <p class="mb-6">ぜひ、一緒にカンファレンスを楽しみましょう！</p>
+        <p class="text-right">{{ representative }}</p>
       </div>
     </div>
   </section>
 </template>
-
-<script setup lang="ts">
-import { message, representative } from '~/utils/constants'
-</script>
