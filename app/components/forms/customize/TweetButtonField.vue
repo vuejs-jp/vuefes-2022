48445790fe3cb5ea9fe4b3126b50617685ec5453
Kohei Tsuchiya <doyahirohira@gmail.com>
2022-03-30 23:52:01 +0900

    メインビジュアルのTweetボタンの一部を太字にして、Transitionを付けた

--- app/components/forms/customize/TweetButtonField.vue
+++ app/components/forms/customize/TweetButtonField.vue
@@ -5,12 +5,12 @@ interface ITweetButtonFieldEmit {
   (e: 'onClick'): void
 }
 
-const props = defineProps({
-  titleLabel: {
-    type: String,
-    required: true,
-  },
-})
+// const props = defineProps({
+//   titleLabel: {
+//     type: String,
+//     required: true,
+//   },
+// })
 const emit = defineEmits<ITweetButtonFieldEmit>()
 
 const handleClick = () => {
@@ -21,13 +21,13 @@ const handleClick = () => {
 <template>
   <button
     type="submit"
-    class="w-[56.62vw] h-[9.48vw] text-[3.38vw] text-center text-vue-blue bg-transparent rounded-4xl border border-vue-blue hover:opacity-80 md:w-[48.98vw] md:h-[6.43vw] md:text-[2.45vw] lg:w-[26.875rem] lg:h-[3.9375rem] lg:text-[1.5rem]"
+    class="w-[56.62vw] h-[9.48vw] text-[3.38vw] text-center text-vue-blue bg-transparent rounded-4xl border border-vue-blue hover:opacity-60 transition-opacity md:w-[48.98vw] md:h-[6.43vw] md:text-[2.45vw] lg:w-[26.875rem] lg:h-[3.9375rem] lg:text-[1.5rem]"
     @click="handleClick"
   >
     <div class="flex justify-center items-center align-middle">
-      <TwitterSvg />
-      <span class="ml-4">
-        {{ titleLabel }}
+      <TwitterSvg class="w-[3.64vw]" />
+      <span class="ml-[1.82vw] lg:ml-1">
+        <span class="font-bold">Twitter</span> ー @vuefes #vuefes
       </span>
     </div>
   </button>
