89b1ac15c6d3c3f72159c76ff4a135d926d8ba34
jiyuujin <jiyuujinunite@gmail.com>
2022-04-06 13:47:56 +0900

    delete: 余分なコードをば

--- scripts/renameDir.js
+++ scripts/renameDir.js
@@ -1,16 +1,12 @@
 const fs = require('fs-extra')
 const { resolve } = require('path')
 
-fs.mkdir('tmp', (err) => {
-  if (err) { throw err }
-})
 fs.moveSync(resolve(__dirname, '../dist/'), resolve(__dirname, '../tmp/'))
 
-fs.mkdir('tmp/2022', (err) => {
-  if (err) { throw err }
-})
 fs.moveSync(resolve(__dirname, '../dist/2022/'), resolve(__dirname, '../tmp/2022/'))
 
-fs.rmdir('tmp', (err) => {
-  if (err) { throw err }
+fs.remove('tmp', (err) => {
+  if (err) {
+    throw err
+  }
 })
