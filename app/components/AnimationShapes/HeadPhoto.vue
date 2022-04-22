1f32e09ac3546ee74ba670e6207538fa467509db
Kohei Tsuchiya <doyahirohira@gmail.com>
2022-04-23 01:44:49 +0900

    メインビジュアルの画像をBase64エンコードして埋め込むように修正

--- app/components/AnimationShapes/HeadPhoto.vue
+++ app/components/AnimationShapes/HeadPhoto.vue
@@ -18,8 +18,59 @@
       />
     </clipPath>
     <image
+      v-show="parts.src == 'image01.png'"
       key="1"
-      :href="`/img/animation/${parts.src}`"
+      :href="images[0]"
+      x="-60"
+      y="-60"
+      width="120"
+      height="120"
+      :clip-path="`url(#${CLIP_ID})`"
+    />
+    <image
+      v-show="parts.src == 'image02.png'"
+      key="2"
+      :href="images[1]"
+      x="-60"
+      y="-60"
+      width="120"
+      height="120"
+      :clip-path="`url(#${CLIP_ID})`"
+    />
+    <image
+      v-show="parts.src == 'image03.png'"
+      key="3"
+      :href="images[2]"
+      x="-60"
+      y="-60"
+      width="120"
+      height="120"
+      :clip-path="`url(#${CLIP_ID})`"
+    />
+    <image
+      v-show="parts.src == 'image04.png'"
+      key="4"
+      :href="images[3]"
+      x="-60"
+      y="-60"
+      width="120"
+      height="120"
+      :clip-path="`url(#${CLIP_ID})`"
+    />
+    <image
+      v-show="parts.src == 'image05.png'"
+      key="5"
+      :href="images[4]"
+      x="-60"
+      y="-60"
+      width="120"
+      height="120"
+      :clip-path="`url(#${CLIP_ID})`"
+    />
+    <image
+      v-show="parts.src == 'image06.png'"
+      key="6"
+      :href="images[5]"
       x="-60"
       y="-60"
       width="120"
@@ -31,6 +82,13 @@
 <script lang="ts">
 import type { CustomPropType } from '~/types/shims-vue'
 import { useAnimationParts } from '~/composables/useAnimationParts'
+
+import Image01 from '!url-loader!~/assets/img/animation/image01.png'
+import Image02 from '!url-loader!~/assets/img/animation/image02.png'
+import Image03 from '!url-loader!~/assets/img/animation/image03.png'
+import Image04 from '!url-loader!~/assets/img/animation/image04.png'
+import Image05 from '!url-loader!~/assets/img/animation/image05.png'
+import Image06 from '!url-loader!~/assets/img/animation/image06.png'
 
 interface IPropParts {
   type: string
@@ -46,8 +104,8 @@ export default {
   props: {
     parts: {
       type: Object as CustomPropType<IPropParts>,
-      required: true
-    }
+      required: true,
+    },
   },
 
   setup(props, setupContext) {
@@ -58,6 +116,8 @@ export default {
     const CLIP_ID = `photo-clip${props.parts.key}`
 
     const transform = useAnimeParts.transform(props)
+
+    const images = [Image01, Image02, Image03, Image04, Image05, Image06]
 
     const createAnimations = () => {
       setTimeout(() => {
@@ -77,7 +137,7 @@ export default {
       fadeAnimations()
     })
 
-    return { transform, CLIP_ID }
-  }
+    return { transform, CLIP_ID, images }
+  },
 }
 </script>
