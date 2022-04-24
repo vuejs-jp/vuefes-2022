9cad4033a68c2d5edc1e627ef6b559515cbf481f
Keita <fast.break.baske03@gmail.com>
2022-04-24 16:09:26 +0900

    update:feature/sponsors-section グラデーションイメージの追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -46,6 +46,11 @@ module.exports = {
         'linear-gradient(45deg, #35495e 25%, transparent 25%, transparent 75%, #35495e 75%), linear-gradient(45deg, #35495e 25%, transparent 25%, transparent 75%, #35495e 75%)',
       'ichimatsu-white':
         'linear-gradient(45deg, #fff 25%, transparent 25%, transparent 75%, #fff 75%), linear-gradient(45deg, #fff 25%, transparent 25%, transparent 75%, #fff 75%)',
+      'ichimatsu-hiwamoegi':
+        'linear-gradient(45deg, #90b44b 25%, transparent 25%, transparent 75%, #90b44b 75%), linear-gradient(45deg, #90b44b 25%, transparent 25%, transparent 75%, #90b44b 75%)',
+      'ichimatsu-silver-tree':
+        'linear-gradient(45deg, #5ab088 25%, transparent 25%, transparent 75%, #5ab088 75%), linear-gradient(45deg, #5ab088 25%, transparent 25%, transparent 75%, #5ab088 75%)',
+      suponsors: 'linear-gradient(180deg, #90b44b 0%, #5ab088 100%)',
       submit: 'linear-gradient(91.03deg, #33A6B8 0%, #42B983 100.01%)',
       'submit-border': 'linear-gradient(90deg, #42b983 0%, #33a6b8 100%)',
     },
