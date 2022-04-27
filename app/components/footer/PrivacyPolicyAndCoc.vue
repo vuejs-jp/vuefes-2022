18bdc8e385f251936fb27d7e71afce68ba518760
ryuhei373 <ryuhei.373_3@gmail.com>
2022-04-27 21:25:46 +0900

    プライバシーポリシーページへのリンクと空ページ

---
+++ app/components/footer/PrivacyPolicyAndCoc.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { websiteLinks } from '~/utils/constants'
+</script>
+
+<template>
+  <div class="flex gap-8 justify-center">
+    <nuxt-link            
+      to="/privacy"
+      class="text-xs leading-4 hover:text-gray-500 underline focus:underline focus:outline-none transition cursor-pointer lg:text-lg"
+    >
+      プライバシーポリシー
+    </nuxt-link>
+    <nuxt-link            
+      to="/code-of-conduct"
+      class="text-xs leading-4 hover:text-gray-500 underline focus:underline focus:outline-none transition cursor-pointer lg:text-lg"
+    >
+      行動規範
+    </nuxt-link>
+  </div>
+</template>
---
+++ app/pages/privacy.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <nav-page-section />
+  </div>
+</template>
+
+<script setup lang="ts">
+import NavPageSection from '~/components/NavPageSection.vue'
+import SubmitButtonField from '~/components/forms/customize/SubmitButtonField.vue'
+</script>
