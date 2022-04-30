5902e814f1d0389b507f846bef61be827036c966
ryuhei373 <ryuhei.373_3@gmail.com>
2022-04-30 22:48:48 +0900

    h2 のサイズ調整

--- app/pages/code-of-conduct.vue
+++ app/pages/code-of-conduct.vue
@@ -19,16 +19,16 @@ import FooterPageSection from '~/components/FooterPageSection.vue'
     <div class="px-5 mx-auto max-w-[43.75rem] text-sm leading-7 text-vue-blue lg:max-w-[73.75rem] lg:text-lg lg:leading-8">
       <p class="mb-16">Vue Fes Japan Online 2022 のすべての参加者、スピーカー、スポンサー、スタッフは、オープンかつ友好的な環境を育むため、以下の行動規範に同意していただく必要があります。主催者である Vue.js 日本ユーザーグループは、この規範の実施を遵守します。誰にとっても安全な環境を守るために、すべての参加者のご協力をお願いいたします。</p>
 
-      <h2 class="mb-8 text-3xl font-bold">差別、ハラスメント行為の禁止</h2>
+      <h2 class="mb-8 text-xl font-bold lg:text-3xl">差別、ハラスメント行為の禁止</h2>
       <p class="mb-8">年齢、外見、身体の大きさ、性別、性的指向、障害の有無、人種、民族、国籍、宗教、学歴、社会的地位、技術選択、経験の度合いに関わらず、すべての参加者が安全に参加できる環境の提供に努め、いかなる差別も容認しません。</p>
       <p class="mb-8">性的な表現、不適切な身体接触、暴言、挑発的または侮蔑的な表現、意図的な威力行為、脅迫、ストーカー行為、つきまとい、写真撮影や録音などによる嫌がらせ、トークやその他のイベントに対して繰り返して意図的に中断させる行為といったハラスメント行為を一切容認しません。</p>
       <p class="mb-16">これらの差別、ハラスメント行為は会話、セッション、スポンサーブース、パーティ、SNS など、Vue Fes Japan Online 2022 に関連するあらゆる場面、場所に不適切です。意図して行ったかどうかに関わらず、Vue.js 日本ユーザーグループの判断でこれらの行為を止めるように求められた場合、すみやかに従わなければなりません。指示に従わない場合、Vue Fes Japan Online 2022 内での行動を制限、または参加費の払い戻しなく退席をしていただく場合があります。</p>
 
-      <h2 class="mb-8 text-3xl font-bold">いやな思いをしてしまったら</h2>
+      <h2 class="mb-8 text-xl font-bold lg:text-3xl">いやな思いをしてしまったら</h2>
       <p class="mb-8">もしあなたが差別やハラスメント行為を受けたと感じた場合や、誰かがハラスメント行為を受けていることに気づいた場合、またはその懸念がある場合は、すぐにお近くのスタッフまでご連絡ください。</p>
       <p class="mb-16">Vue.js 日本ユーザーグループは安全な環境を守るため、警備員や警察への連絡を含め、付き添い人の提供、その他のあらゆる支援を行います。私達にとってあなたの参加が何よりも大切なことであり、常に差別、ハラスメント行為を受けた人の味方であり続けます。</p>
 
-      <h2 class="mb-8 text-3xl font-bold">何かお困りですか？</h2>
+      <h2 class="mb-8 text-xl font-bold lg:text-3xl">何かお困りですか？</h2>
       <p class="mb-8">
         Vue.js 日本ユーザーグループへのお問い合わせは、<nuxt-link            
           :to="{ path: '/',hash:'#form'}"
@@ -49,7 +49,3 @@ import FooterPageSection from '~/components/FooterPageSection.vue'
     <footer-page-section />
   </div>
 </template>
-
-
-:to="{ path: '/',hash:'#form'}"
-
--- app/pages/privacy.vue
+++ app/pages/privacy.vue
@@ -20,10 +20,10 @@ import FooterPageSection from '~/components/FooterPageSection.vue'
       <p class="mb-8">Vue Fes Japan Online 2022（以下、 カンファレンス）では、カンファレンスへの参加者およびスポンサー（以下、ユーザー）の個人情報保護の重要性について認識しています。</p>
       <p class="mb-16">個人情報の保護に関する法律を遵守できるよう、以下のプライバシーポリシー（以下、本プライバシーポリシー）に従い、適切な取扱い、保護そして継続的改善に努めます。</p>
 
-      <h2 class="mb-8 text-3xl font-bold">個人情報の定義</h2>
+      <h2 class="mb-8 text-xl font-bold lg:text-3xl">個人情報の定義</h2>
       <p class="mb-16">本プライバシーポリシーにおいて、「個人情報」とは、個人情報保護法にいう、生存する個人に関する情報であり、当該情報に含まれる氏名、生年月日その他の記述等により特定の個人を識別することができるもの（他の情報と容易に照合することができ、それにより特定の個人を識別することができることとなるものを含みます）、もしくは個人識別符号が含まれる情報を意味するものとします。</p>
 
-      <h2 class="mb-8 text-3xl font-bold">個人情報の収集</h2>
+      <h2 class="mb-8 text-xl font-bold lg:text-3xl">個人情報の収集</h2>
       <p class="mb-8">当カンファレンスは、個人情報の入手を適法かつ公正な手段によって行うものとし、ユーザーの意思に反する不正な入手を行わないものとします。</p>
       <p class="mb-8">当カンファレンスでは、ユーザーの以下の個人情報を取り扱います。</p>
       <ul class="pl-12 mb-16 list-disc">
@@ -44,7 +44,7 @@ import FooterPageSection from '~/components/FooterPageSection.vue'
         </li>
       </ul>
 
-      <h2 class="mb-8 text-3xl font-bold">個人情報の利用目的</h2>
+      <h2 class="mb-8 text-xl font-bold lg:text-3xl">個人情報の利用目的</h2>
       <p class="mb-8">当カンファレンスは、ユーザーから収集した個人情報を、以下の目的で利用するものとします。</p>
       <ul class="pl-12 mb-16 list-decimal">
         <li>当カンファレンスの更新情報をご提供する場合</li>
@@ -55,19 +55,19 @@ import FooterPageSection from '~/components/FooterPageSection.vue'
         <li>当カンファレンスに関する規約等の変更などを通知する場合</li>
       </ul>
 
-      <h2 class="mb-8 text-3xl font-bold">個人情報利用目的の変更</h2>
+      <h2 class="mb-8 text-xl font-bold lg:text-3xl">個人情報利用目的の変更</h2>
       <p class="mb-16">当カンファレンスは、個人情報の利用目的を、関連性を有すると合理的に認められる範囲内において変更することがあり、変更した場合には当カンファレンスに関わる皆様に通知又は公表します。本プライバシーポリシーの変更は、本プライバシーポリシーを本ウェブサイト上に掲載した時点で効力を生じるものとします。</p>
       
-      <h2 class="mb-8 text-3xl font-bold">個人情報の利用期限</h2>
+      <h2 class="mb-8 text-xl font-bold lg:text-3xl">個人情報の利用期限</h2>
       <p class="mb-16">当カンファレンスは、ユーザーから収集した個人情報を開催月の2022年10月31日まで利用するものとします。その利用期限が過ぎましたら、速やかに個人情報を完全に削除します。</p>
     
-      <h2 class="mb-8 text-3xl font-bold">個人情報の安全管理</h2>
+      <h2 class="mb-8 text-xl font-bold lg:text-3xl">個人情報の安全管理</h2>
       <p class="mb-16">当カンファレンスは、個人情報の紛失、破壊、改ざん及び漏洩などのリスクに対して、個人情報の安全管理が図られるよう、Vue.js 日本ユーザーグループおよびその関係者に対し、必要かつ適切な監督を行います。</p>
   
-      <h2 class="mb-8 text-3xl font-bold">個人情報の委託</h2>
+      <h2 class="mb-8 text-xl font-bold lg:text-3xl">個人情報の委託</h2>
       <p class="mb-16">当カンファレンスは、業務運営上、お客様により良いサービスを提供するために、業務の一部を外部に委託することがあります。その際に業務委託先に個人情報を預けることがあります。この場合、十分な個人情報の保護の水準を満たしている委託先を選定し、個人情報の保護に関する委託契約を締結すると共に、委託先に対する管理・監督を実施いたします。</p>
 
-      <h2 class="mb-8 text-3xl font-bold">個人情報の第三者提供</h2>
+      <h2 class="mb-8 text-xl font-bold lg:text-3xl">個人情報の第三者提供</h2>
       <p class="mb-8">当カンファレンスは、収集した個人情報を個人情報保護法に基づき業務委託先以外の第三者に対して開示又は提供しないものとします。ただし、以下に該当する場合は、この限りではありません。</p>
       <ul class="pl-12 mb-16 list-disc">
         <li>法令により開示が認められている場合</li>
@@ -76,16 +76,16 @@ import FooterPageSection from '~/components/FooterPageSection.vue'
         <li>警察、検察、裁判所等公的機関等によって開示を求められた場合</li>
       </ul>
 
-      <h2 class="mb-8 text-3xl font-bold">個人情報等の最新性、正確性の維持</h2>
+      <h2 class="mb-8 text-xl font-bold lg:text-3xl">個人情報等の最新性、正確性の維持</h2>
       <p class="mb-16">当カンファレンスは、利用目的の達成に必要な範囲内において、ユーザーの個人情報等を正確かつ最新の内容に保つよう努めるものとします。</p>
 
-      <h2 class="mb-8 text-3xl font-bold">個人情報の照会、訂正、削除等</h2>
+      <h2 class="mb-8 text-xl font-bold lg:text-3xl">個人情報の照会、訂正、削除等</h2>
       <p class="mb-16">当カンファレンスは、ユーザーより当該ユーザーの個人情報の照会、訂正、追加、削除、利用の停止又は消去を求められた場合、本人確認を行うものとします。なお、当カンファレンスは、ユーザー本人による照会等であると当カンファレンスが判断した場合、合理的な期間内に同照会等に対応するものとします。</p>
 
-      <h2 class="mb-8 text-3xl font-bold">Cookie または類似技術の利用と目的</h2>
+      <h2 class="mb-8 text-xl font-bold lg:text-3xl">Cookie または類似技術の利用と目的</h2>
       <p class="mb-16">当カンファレンスのウェブサイトは、Cookie 及び類似技術を利用します。これらの技術で、本ウェブサイトの利便性の向上および利用状況を分析するために、Cookie を利用して情報を収集しています。</p>
 
-      <h2 class="mb-8 text-3xl font-bold">使用する Third Party Cookie</h2>
+      <h2 class="mb-8 text-xl font-bold lg:text-3xl">使用する Third Party Cookie</h2>
       <p class="mb-8">当カンファレンスでは、本ウェブサイトの利用状況や利便性を向上させるために、Third Party が提供するサービスを利用しており、そのThird Party によって Cookie が設定されることがあります。当カンファレンスで利用している Third Party のサービスは以下のとおりです。</p>
       <ul class="pl-12 mb-8 list-disc">
         <li>Google Analytics</li>
@@ -104,25 +104,25 @@ import FooterPageSection from '~/components/FooterPageSection.vue'
         </li>
       </ul>
 
-      <h2 class="mb-8 text-3xl font-bold">Cookie の削除又は無効化</h2>
+      <h2 class="mb-8 text-xl font-bold lg:text-3xl">Cookie の削除又は無効化</h2>
       <p class="mb-8">ほとんどのウェブブラウザでは自動的に Cookie を受信するよう初期設定されていますが、ユーザー自身で Cookie を拒否したり、Cookie が送信された際に警告が表示されたりするように設定変更できます。Cookie の管理にはいくつもの方法があります。</p>
       <p class="mb-8">ウェブブラウザ設定の調整、変更に関する詳細は、お使いのウェブブラウザのヘルプ画面等をご覧ください。なお、ユーザーが当カンファレンスのウェブサイトで使用する Cookie を無効化すると、本ウェブサイトの特定箇所にアクセスできなかったり、本ウェブサイトの全部又は一部が利用できなくなったりする等、本ウェブサイトご利用中の動作に影響を及ぼす可能性があります。予めご了承ください。</p>
       <p class="mb-16">本ウェブサイト上における Cookie の保存期間は、ユーザーが本ウェブサイトに最後に訪問した日から最長で 2 年間です。</p>
 
-      <h2 class="mb-8 text-3xl font-bold">継続的改善</h2>
+      <h2 class="mb-8 text-xl font-bold lg:text-3xl">継続的改善</h2>
       <p class="mb-16">当カンファレンスでは、個人情報の取扱いに関する運用状況を適宜見直し、継続的な改善に努めるものとし、必要に応じて、本プライバシーポリシーを変更することがあります。</p>
 
-      <h2 class="mb-8 text-3xl font-bold">お問い合わせ</h2>
+      <h2 class="mb-8 text-xl font-bold lg:text-3xl">お問い合わせ</h2>
       <p class="mb-16">
         開示等のお申出、ご意見、ご質問、苦情のお申出その他個人情報の取扱いに関するお問い合わせは、メールにて <a
+          target="_blank"
           rel="noreferrer"
           href="mailto:vuefes+privacy@gmail.com"
           class="hover:text-gray-500 underline focus:underline focus:outline-none transition cursor-pointer"
         >vuefes+privacy@gmail.com</a> にお問い合わせください。
       </p>
 
-
-      <h2 class="mb-8 text-3xl font-bold">附則</h2>
+      <h2 class="mb-8 text-xl font-bold lg:text-3xl">附則</h2>
       <p class="mb-8">当カンファレンスは、収集した個人情報を個人情報保護法に基づき業務委託先以外の第三者に対して開示又は提供しないものとします。ただし、以下に該当する場合は、この限りではありません。</p>
       <ul class="pl-12 mb-16 list-disc">
         <li>2022年4月28日 制定・施行</li>
