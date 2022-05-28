51100740550b993a52349489b13d0a8841bb9748
ryuhei373 <ryuhei.373_3@gmail.com>
2022-05-28 14:18:55 +0900

    lint

--- app/components/SpeakerPageHeading.vue
+++ app/components/SpeakerPageHeading.vue
@@ -15,7 +15,7 @@ const props = defineProps({
 
 <style>
 .bg-gradient {
-  background: rgb(255,255,255);
-  background: linear-gradient(90deg, rgba(255,255,255,1) 0%, rgba(255,255,255,0) 100%);
+  background: rgb(255 255 255);
+  background: linear-gradient(90deg, rgb(255 255 255 / 100%) 0%, rgb(255 255 255 / 0%) 100%);
 }
 </style>
