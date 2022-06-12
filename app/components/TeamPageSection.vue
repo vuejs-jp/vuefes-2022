9a389330dd431743fba062857879292298ba2ba3
Kohei Tsuchiya <doyahirohira@gmail.com>
2022-06-12 17:31:26 +0900

    チームセクションのマークアップを調整

--- app/components/TeamPageSection.vue
+++ app/components/TeamPageSection.vue
@@ -14,47 +14,43 @@ const showStaffs = computed(() => staffList.filter((staff) => staff.image !== ''
     id="team"
     class="py-10 px-5 bg-staffs md:px-10 lg:p-20"
   >
-    <div class="py-20 bg-white lg:py-40">
-      <div class="px-5 mx-auto max-w-[83.75rem] md:max-w-[92.25rem]">
-        <SectionTitle
-          class="mb-10 lg:mb-20"
-          title="Team"
-          title-yamato="チーム"
-        />
-        <p class="px-4 pt-20 pb-14 text-center md:px-14">
-          Vue Fes Japan Online 2022 は、Vue.js 日本ユーザーグループのスタッフによって企画・運営されています。
-        </p>
-        <div class="mb-10 text-center lg:mb-20">
-          <div class="flex flex-wrap justify-center items-center w-full">
-            <div
-              v-for="(staff, index) in showStaffs"
-              :key="index"
-              class="flex flex-col justify-center mx-1 mb-10 md:mx-4 w-1/8"
+    <div class="py-20 px-5 bg-white lg:py-40">
+      <SectionTitle
+        class="mb-10 lg:mb-20"
+        title="Team"
+        title-yamato="チーム"
+      />
+      <p class="mb-10 text-sm leading-7 md:text-center lg:mb-14 lg:text-lg lg:leading-8">
+        Vue Fes Japan Online 2022 は、Vue.js 日本ユーザーグループのスタッフによって企画・運営されています。
+      </p>
+      <div class="grid grid-cols-3 gap-3 mx-auto max-w-[1180px] md:grid-cols-5 lg:grid-cols-7">
+        <div
+          v-for="(staff, index) in showStaffs"
+          :key="index"
+        >
+          <template v-if="staff.twitterID">
+            <a
+              :href="`https://twitter.com/${staff.twitterID}`"
+              :title="`${staff.name} logo`"
+              target="_blank"
+              rel="noopener"
             >
-              <a
-                v-if="staff.twitterID"
-                :href="`https://twitter.com/${staff.twitterID}`"
-                :title="`${staff.name} logo`"
-                target="_blank"
-                rel="noopener"
+              <img
+                :alt="`${staff.name} logo`"
+                :src="`/staff/${staff.image}`"
+                class="mb-1 w-full"
               >
-                <img
-                  :alt="`${staff.name} logo`"
-                  :src="`/staff/${staff.image}`"
-                  class="w-20 h-20 md:w-[7.2rem] md:h-[7.2rem]"
-                >
-                <div class="pt-2 text-sm text-center">{{ staff.name }}</div>
-              </a>
-              <div v-else>
-                <img
-                  :alt="`${staff.name} logo`"
-                  :src="`/staff/${staff.image}`"
-                  class="w-20 h-20 md:w-[7.2rem] md:h-[7.2rem]"
-                >
-                <div class="pt-2 text-sm text-center">{{ staff.name }}</div>
-              </div>
-            </div>
-          </div>
+              <p class="text-sm text-center">{{ staff.name }}</p>
+            </a>
+          </template>
+          <template v-else>
+            <img
+              :alt="`${staff.name} logo`"
+              :src="`/staff/${staff.image}`"
+              class="mb-1 w-full"
+            >
+            <p class="text-sm text-center">{{ staff.name }}</p>
+          </template>
         </div>
       </div>
     </div>
