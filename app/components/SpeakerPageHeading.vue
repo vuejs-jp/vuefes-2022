58cd051a5de78ec8af3f9b6897b4db44f59386e5
Yuma Kitamura <jiyuujinunite@gmail.com>
2022-06-21 08:38:42 +0900

    Revert "refactor: add custom configuration"

--- app/components/SpeakerPageHeading.vue
+++ app/components/SpeakerPageHeading.vue
@@ -8,7 +8,14 @@ const props = defineProps({
 </script>
 
 <template>
-  <h3 class="py-3 px-4 text-base font-bold text-vue-blue bg-speakers lg:py-5 lg:px-7 lg:text-2xl">
+  <h3 class="py-3 px-4 text-base font-bold text-vue-blue lg:py-5 lg:px-7 lg:text-2xl bg-gradient">
     {{ text }}
   </h3>
 </template>
+
+<style scoped>
+.bg-gradient {
+  background: rgb(255 255 255);
+  background: linear-gradient(90deg, rgb(255 255 255 / 100%) 0%, rgb(255 255 255 / 0%) 100%);
+}
+</style>
