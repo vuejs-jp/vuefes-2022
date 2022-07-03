2dac311eb2d2a757e58fc27c1cb12e56e5f38de5
Kohei Tsuchiya <doyahirohira@gmail.com>
2022-07-03 12:40:36 +0900

    スピーカーセクションの余白を微修正

--- app/components/SpeakerLTSection.vue
+++ app/components/SpeakerLTSection.vue
@@ -1,9 +1,9 @@
 <template>
-  <section class="p-4 mt-20 border border-vue-blue lg:p-10">
-    <p class="mx-auto -mt-[2.0rem] mb-8 w-3/5 text-xl font-bold text-center text-vue-blue lg:-mt-[3.6rem] lg:text-3xl bg-img">
+  <section class="p-4 border border-vue-blue md:px-10 lg:py-10 lg:px-20">
+    <h3 class="mx-auto -mt-[2.0rem] mb-6 w-[14.375rem] text-xl font-bold text-center text-vue-blue md:mb-8 lg:-mt-[3.6rem] lg:mb-14 lg:w-[26.875rem] lg:text-4xl bg-img">
       LT募集について
-    </p>
-    <p class="mb-4 text-sm leading-7 text-vue-blue lg:mb-8 lg:text-lg lg:leading-8">
+    </h3>
+    <p class="mb-8 text-sm leading-7 text-vue-blue lg:mb-12 lg:text-lg lg:leading-8">
       Vue Fes Japan Online 2022 ではLT発表者を募集します。
       テーマは「Vue または Vue を取り巻くエコシステムに関する内容」、登壇時間は5分です。
       Vueとそのエコシステムについて、あなたが持つ知見と経験を、日本最大級の Vue.js カンファレンスでぜひ日本中に共有してください。
@@ -16,17 +16,17 @@
       Vue Fes Japan Online 2022 では、LT発表枠を広く一般から募集するため、CFP（Call for Papers）の仕組みを利用します。
       Vue.js に関する具体的な登壇内容をご応募いただき、Vue Fes Japan Online 2022 実行委員会が選考の上、採用します。
     </p>
-    <p class="mb-4 text-sm leading-7 text-vue-blue lg:mb-8 lg:text-lg lg:leading-8">
+    <p class="mb-8 text-sm leading-7 text-vue-blue lg:mb-12 lg:text-lg lg:leading-8">
       ※応募には Fortee アカウントの作成が必要です。
     </p>
     <SpeakerPageHeading
       class="mb-4 lg:mb-8"
       text="募集スケジュール"
     />
-    <p class="mb-4 text-sm leading-7 text-vue-blue lg:mb-8 lg:text-lg lg:leading-8">
+    <p class="mb-12 text-sm leading-7 text-vue-blue lg:mb-16 lg:text-lg lg:leading-8">
       応募期限：2022/7/31（日）23:59:59 まで
     </p>
-    <div class="mb-8 text-center lg:mb-16">
+    <div class="mb-6 text-center">
       <LinkButtonField
         link="https://fortee.jp/vuefes-2022/speaker/callfor/lightning-talk/callfor"
         title-label="LTを応募する"
