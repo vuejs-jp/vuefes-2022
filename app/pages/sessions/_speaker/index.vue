e0d0b1979d56cf42482630edd97d5c9266569d59
yuma-kitamura <jiyuujinunite@gmail.com>
2022-07-21 20:39:05 +0900

    refactor: OG 取得のロジックを整理

--- app/pages/sessions/_speaker/index.vue
+++ app/pages/sessions/_speaker/index.vue
@@ -5,6 +5,8 @@ import SessionSection from '~/components/SessionSection.vue'
 import FooterPageSection from '~/components/FooterPageSection.vue'
 import IchimatsuDividedBar from '~/components/IchimatsuDividedBar.vue'
 import { speakers } from '~/utils/speakers.constants'
+import { generalOg, twitterOg } from '~/utils/og.constants'
+import { conferenceTitle, linkUrl } from '~/utils/constants'
 
 const route = useRoute()
 
@@ -13,35 +15,15 @@ const speaker = computed(() => {
 })
 
 const url = `https://vuefes.jp/2022/sessions/${speaker.value.id}`
-const title = `${speaker.value.session.title}（${speaker.value.name}） | Vue Fes Japan Online 2022`
-const description = `Vue Fes Japan Online 2022 のセッション情報です。スピーカーの ${speaker.value.name} が、「${speaker.value.session.title}」を発表します。`
-const ogImageUrl = `https://vuefes.jp/2022/speaker-og-images/${speaker.value.id}.jpg`
+const title = `${speaker.value.session.title}（${speaker.value.name}） | ${conferenceTitle}`
+const description = `${conferenceTitle} のセッション情報です。スピーカーの ${speaker.value.name} が、「${speaker.value.session.title}」を発表します。`
+const ogImageUrl = `${linkUrl}speaker-og-images/${speaker.value.id}.jpg`
 
 useNuxt2Meta({
   title,
   meta: [
-    { hid: 'description', name: 'description', content: description },
-    { hid: 'og:url', name: 'og:url', content: url },
-    { hid: 'og:title', name: 'og:title', content: title },
-    { hid: 'og:description', name: 'og:description', content: description },
-    { hid: 'og:image', name: 'og:image', content: ogImageUrl },
-    {
-      hid: 'og:image:secure_url',
-      name: 'og:image:secure_url',
-      content: ogImageUrl
-    },
-    {
-      hid: 'twitter:card',
-      name: 'twitter:card',
-      content: 'summary_large_image'
-    },
-    {
-      hid: 'twitter:description',
-      name: 'twitter:description',
-      content: description
-    },
-    { hid: 'twitter:title', name: 'twitter:title', content: title },
-    { hid: 'twitter:image', name: 'twitter:image', content: ogImageUrl }
+    ...generalOg(title, description, url, ogImageUrl),
+    ...twitterOg(title, description, ogImageUrl),
   ]
 })
 </script>
