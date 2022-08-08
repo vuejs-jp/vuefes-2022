abe3ed58ac95350da1e0ee066fbaa8dff71bc0c2
jiyuujin <jiyuujinunite@gmail.com>
2022-08-08 21:32:01 +0900

    update background

--- app/components/navbar/NavMenu.vue
+++ app/components/navbar/NavMenu.vue
@@ -33,7 +33,7 @@ const localIsOpen = computed({
     </div>
     <nav
       v-if="localIsOpen"
-      class="absolute inset-x-0 top-[13.51vw] bottom-0 z-10 w-full h-screen bg-white/90"
+      class="absolute inset-x-0 top-[13.51vw] bottom-0 z-10 w-full h-screen bg-nav"
     >
       <ul class="flex flex-col space-y-6 ">
         <li
