29f1513298e778fe3f83a8c2f25dabfca1713160
James Roberts <robjam.github@gmail.com>
2022-08-29 00:22:01 +0900

    redo mobile ui

--- app/components/TimeTableSection.vue
+++ app/components/TimeTableSection.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
 import SectionTitle from '~/components/SectionTitle.vue'
+
+import TrackLabel from '~/components/timetable/TrackLabel.vue'
 function trackClassByIndex(index:number, isSponsorEvent: boolean) {
   switch (index) {
   case 0:
-    return isSponsorEvent ? 'border border-track-a text-track-a' : 'bg-sponsor/[.03] border-t-2 border-track-a'
+    return isSponsorEvent ? 'border border-track-a text-track-a items-start sm:items-center' : 'bg-sponsor/[.03] border-t-2 border-track-a'
   case 1:
-    return isSponsorEvent ?  'border border-track-b text-track-b' : 'bg-sponsor/[.03] border-t-2 border-track-b'
+    return isSponsorEvent ?  'border border-track-b text-track-b items-start sm:items-center' : 'bg-sponsor/[.03] border-t-2 border-track-b'
   case 2:
-    return isSponsorEvent ?  'border border-track-c text-track-c' : 'bg-sponsor/[.03] border-t-2 border-track-c'
+    return isSponsorEvent ?  'border border-track-c text-track-c items-start sm:items-center' : 'bg-sponsor/[.03] border-t-2 border-track-c'
     
   default:
     break
@@ -168,87 +170,160 @@ const timeslots = [
 <template>
   <section
     id="timetable"
-    class="py-10 px-5 bg-timetable md:px-10 lg:p-20"
+    class="py-10 px-5 bg-timetable md:px-10 lg:p-20 text-timetable"
   >
-    <div class="relative justify-center p-20 text-center bg-white md:py-12 xl:px-0">
-      <SectionTitle
-        class="mb-10 lg:mb-20"
-        title="Time Table"
-        title-yamato="タイムテーブル"
-      />
-      <div class="grid grid-cols-1 gap-2.5 mx-auto mb-2.5 md:sticky md:top-0 md:grid-cols-md-timetable xl:grid-cols-timetable xl:max-w-[1260px]">
-        <div class="py-7 " />
-        <div class="flex justify-center items-center py-4 px-2 text-lg font-bold text-white bg-track-a lg:text-2xl">メドピアトラック</div>
-        <div class="flex justify-center items-center py-4 px-2 text-lg font-bold text-white bg-track-b lg:text-2xl">FUTUREトラック</div>
-        <div class="flex justify-center items-center py-4 px-2 text-lg font-bold text-white bg-track-c lg:text-2xl">クラウドサイントラック</div>
-      </div>
-      <div
-        v-for="(timeslot, index) in timeslots"
-        :key="index"
-        class="grid grid-cols-1 gap-y-2.5 mx-auto mb-2.5 md:grid-cols-md-timetable md:gap-2.5 xl:grid-cols-timetable xl:max-w-[1260px]"
-      >
-        <p class="py-7 min-h-[5rem] text-lg text-center text-white align-middle bg-timetable-timeslot md:min-h-[5.25rem] lg:text-2xl">
-          {{ timeslot.time }}
-        </p>
-        <template v-if="timeslot.isAllTrackEvent">
-          <div class="flex col-span-3 justify-center items-center px-1 mb-10 min-h-[5rem] text-base text-center bg-sponsor/[.03] md:mb-0 lg:text-lg">
-            <h3>{{ timeslot.events[0].title }}<br>{{ timeslot.events[0].subTitle }}</h3>
-          </div>
-        </template>
-        <template v-else>
-          <div
-            v-for="(tracks, index) in timeslot.events"
-            :key="index"
-            class="flex justify-center items-center px-1 last:mb-10 min-h-[5rem] text-base md:last:mb-0 lg:text-lg"
-            :class="trackClassByIndex(index, timeslot.isSponsorEvent)"
+    <div class="py-20 bg-white lg:py-40">
+      <div class="relative px-5 mx-auto max-w-1190">
+        <SectionTitle
+          class="mb-10 lg:mb-20"
+          title="Time Table"
+          title-yamato="タイムテーブル"
+        />
+        <div class="hidden sm:grid grid-cols-1 gap-2.5 mx-auto mb-2.5 md:sticky md:top-0 md:grid-cols-md-timetable xl:grid-cols-timetable xl:max-w-[1260px]">
+          <div />
+          <div class="flex flex-col justify-center items-center py-4 px-2 font-bold text-white bg-track-a">メドピアトラック</div>
+          <div class="flex flex-col justify-center items-center py-4 px-2 font-bold text-white bg-track-b">FUTUREトラック</div>
+          <div class="flex flex-col justify-center items-center py-4 px-2 font-bold text-white bg-track-c">クラウドサイントラック</div>
+        </div>
+        <div
+          v-for="(timeslot, index) in timeslots"
+          :key="index"
+          class="grid grid-cols-1 gap-y-2.5 mx-auto mb-2.5 md:grid-cols-md-timetable md:gap-2.5 xl:grid-cols-timetable xl:max-w-[1260px]"
+        >
+          <p class="timeslot">
+            {{ timeslot.time }}
+          </p>
+          <template v-if="timeslot.isAllTrackEvent">
+            <div class="flex flex-col col-span-3 justify-center items-center py-3.5 font-bold text-center bg-sponsor/[.03] md:mb-0">
+              <h3 class="">{{ timeslot.events[0].title }}<br>{{ timeslot.events[0].subTitle }}</h3>
+            </div>
+          </template>
+          <template v-else>
+            <TrackLabel
+              v-for="(tracks, timeslotIndex) in timeslot.events"
+              :key="timeslotIndex"
+              class="col-span-3 last:mb-2.5"
+              :is-sponsor-event="timeslot.isSponsorEvent"
+              :track-index="timeslotIndex"
+            >
+              <h3 class="">{{ tracks.title }}<br>{{ tracks.subTitle }}</h3>
+            </TrackLabel>
+          </template>
+        </div>
+        <div
+          class="grid grid-cols-1 gap-y-2.5 mx-auto mb-2.5 md:grid-cols-md-timetable md:gap-2.5 xl:grid-cols-timetable xl:max-w-[1260px]"
+        >
+          <p class="timeslot">
+            15:10 - 15:55
+          </p>
+          <TrackLabel
+            class="col-span-3 row-span-2 justify-center md:col-span-1"
+            :track-index="0"
           >
-            <h3>{{ tracks.title }}<br>{{ tracks.subTitle }}</h3>
-          </div>
-        </template>
-      </div>
-      <div
-        class="grid grid-cols-1 gap-y-2.5 mx-auto mb-2.5 md:grid-cols-md-timetable md:gap-2.5 xl:grid-cols-timetable xl:max-w-[1260px]"
-      >
-        <p class="col-span-3 row-span-2 py-7 min-h-[5rem] text-lg text-center text-white align-middle bg-timetable-timeslot md:col-span-1 lg:text-2xl">
-          15:10 - 15:55
-        </p>
-        <div class="flex col-span-3 row-span-2 justify-center items-center px-1 min-h-[5rem] text-base bg-sponsor/[.03] border-t-2 border-track-a md:col-span-1 lg:text-lg"><h3>ぬるほどVue<br>コンポーネント</h3></div>
-        <div class="flex flex-col col-span-3 row-span-2 justify-center items-center px-1 min-h-[5rem] text-base bg-sponsor/[.03] border-t-2 border-track-b md:col-span-1 lg:text-lg"><p>15:10 - 15:30</p><h3>メインセッション</h3><p class="mt-2.5">15:10 - 15:30</p><h3>メインセッション</h3></div>
-        <div class="flex col-span-3 justify-center items-center px-1 min-h-[5rem] text-base bg-sponsor/[.03] border-t-2 border-track-c md:col-span-1 lg:text-lg"><p>15:10 - 15:30</p><h3>メインセッション</h3></div>
-        <div class="flex col-span-3 row-span-5 justify-center items-center px-1 min-h-[5rem] text-base bg-sponsor/[.03] border-t-2 border-track-c md:col-span-1 lg:text-lg"><p>15:35 - 17:35</p><h3>ハンズオン</h3></div>
+            <h3 class=" text-center">ぬるほどVue<br>コンポーネント</h3>
+          </TrackLabel>
+          <TrackLabel
+            class="col-span-3 row-span-2 md:col-span-1"
+            :track-index="1"
+          >
+            <p>15:10 - 15:30</p><h3 class="">メインセッション</h3><p class="mt-2.5">15:10 - 15:30</p><h3 class="">メインセッション</h3>
+          </TrackLabel>
 
-        <p class="col-span-3 py-7 min-h-[5rem] text-lg text-center text-white align-middle bg-timetable-timeslot md:col-span-1 lg:text-2xl">
-          16:00 - 16:20
-        </p>
-        <div class="flex col-span-3 justify-center items-center px-1 min-h-[5rem] text-base bg-sponsor/[.03] border-t-2 border-track-a md:col-span-1 lg:text-lg"><h3>メインセッション</h3></div>
-        <div class="flex col-span-3 justify-center items-center px-1 min-h-[5rem] text-base bg-sponsor/[.03] border-t-2 border-track-b md:col-span-1 lg:text-lg"><h3>メインセッション</h3></div>
-        
-        <p class="col-span-3 py-7 min-h-[5rem] text-lg text-center text-white align-middle bg-timetable-timeslot md:col-span-1 lg:text-2xl">
-          16:25 - 16:45
-        </p>
-        <div class="flex col-span-3 justify-center items-center px-1 min-h-[5rem] text-base bg-sponsor/[.03] border-t-2 border-track-a md:col-span-1 lg:text-lg"><h3>メインセッション</h3></div>
-        <div class="flex col-span-3 justify-center items-center px-1 min-h-[5rem] text-base border-t-2 border-track-b md:col-span-1 lg:text-lg -sponsor/[.03]"><h3>メインセッション</h3></div>
-        
-        <p class="col-span-3 py-7 min-h-[5rem] text-lg text-center text-white align-middle bg-timetable-timeslot md:col-span-1 lg:text-2xl">
-          16:50 - 17:10
-        </p>
-        <div class="flex col-span-3 justify-center items-center px-1 min-h-[5rem] text-base bg-sponsor/[.03] border-t-2 border-track-a md:col-span-1 lg:text-lg"><h3>メインセッション</h3></div>
-        <div class="flex col-span-3 justify-center items-center px-1 min-h-[5rem] text-base bg-sponsor/[.03] border-t-2 border-track-b md:col-span-1 lg:text-lg"><h3>メインセッション</h3></div>
-        
-        <p class="col-span-3 py-7 min-h-[5rem] text-lg text-center text-white align-middle bg-timetable-timeslot md:col-span-1 lg:text-2xl">
-          17:15 - 17:35
-        </p>
-        <div class="flex col-span-3 justify-center items-center px-1 min-h-[5rem] text-base bg-sponsor/[.03] border-t-2 border-track-a md:col-span-1 lg:text-lg"><h3>メインセッション</h3></div>
-        <div class="flex col-span-3 justify-center items-center px-1 min-h-[5rem] text-base bg-sponsor/[.03] border-t-2 border-track-b md:col-span-1 lg:text-lg"><h3>メインセッション</h3></div>
-        
-        <p class="col-span-3 py-7 min-h-[5rem] text-lg text-center text-white align-middle bg-timetable-timeslot md:col-span-1 lg:text-2xl">
-          18:00 - 19:00
-        </p>
-        
-        <div class="flex col-span-3 justify-center items-center px-1 min-h-[5rem] text-base text-center bg-sponsor/[.03] lg:text-lg">
-          <h3>Peephole</h3>
+          <TrackLabel
+            class="col-span-3 row-span-6 mb-2.5 md:col-span-1"
+            :track-index="2"
+          >
+            <p>15:35 - 17:35</p><h3 class="">ハンズオン</h3>
+          </TrackLabel>
+
+          <p class="timeslot">
+            16:00 - 16:20
+          </p>
+
+
+          <TrackLabel
+            class="col-span-3 md:col-span-1"
+            :track-index="0"
+          >
+            <h3 class="">メインセッション</h3>
+          </TrackLabel>
+          <TrackLabel
+            class="col-span-3 mb-2.5 md:col-span-1"
+            :track-index="1"
+          >
+            <h3 class="">メインセッション</h3>
+          </TrackLabel>
+
+          <p class="timeslot">
+            16:25 - 16:45
+          </p>
+          <TrackLabel
+            class="col-span-3 md:col-span-1"
+            :track-index="0"
+          >
+            <h3 class="">メインセッション</h3>
+          </TrackLabel>
+          <TrackLabel
+            class="col-span-3 mb-2.5 md:col-span-1"
+            :track-index="1"
+          >
+            <h3 class="">メインセッション</h3>
+          </TrackLabel>
+
+          <p class="timeslot">
+            16:50 - 17:10
+          </p>
+          <TrackLabel
+            class="col-span-3 md:col-span-1"
+            :track-index="0"
+          >
+            <h3 class="">メインセッション</h3>
+          </TrackLabel>
+          <TrackLabel
+            class="col-span-3 mb-2.5 md:col-span-1"
+            :track-index="1"
+          >
+            <h3 class="">メインセッション</h3>
+          </TrackLabel>
+
+          <p class="timeslot">
+            17:15 - 17:35
+          </p>
+          <TrackLabel
+            class="col-span-3 md:col-span-1"
+            :track-index="0"
+          >
+            <h3 class="">メインセッション</h3>
+          </TrackLabel>
+          <TrackLabel
+            class="col-span-3 mb-2.5 md:col-span-1"
+            :track-index="1"
+          >
+            <h3 class="">メインセッション</h3>
+          </TrackLabel>
+
+          <p class="timeslot">
+            18:00 - 19:00
+          </p>
+
+          <div class="multi-track">
+            <h3>Peephole</h3>
+          </div>
         </div>
       </div>
     </div>
   </section>
 </template>
+<style scoped>
+.text-timetable {
+  @apply text-sm leading-7 lg:text-lg lg:leading-8
+}
+
+.timeslot {
+  @apply text-sm leading-7 lg:text-lg lg:leading-8 text-white text-center bg-timetable-timeslot col-span-3 md:col-span-1
+}
+
+.multi-track {
+  @apply text-sm leading-7 lg:text-lg lg:leading-8 text-center lg:text-lg col-span-3 bg-sponsor/[0.03]
+}
+</style>
---
+++ app/components/timetable/TrackLabel.vue
@@ -0,0 +1,69 @@
+<script setup lang="ts">
+const props = defineProps({
+  trackIndex: {
+    type: Number as PropType<0 | 1 | 2>,
+    required: true,
+  },
+  isSponsorEvent: {
+    type: Boolean,
+    default: false,
+  },
+})
+
+const trackLabelClasses = computed(() => {
+  switch (props.trackIndex) {
+  case 0:
+    return 'bg-track-a'
+  case 1:
+    return 'bg-track-b'
+  case 2:
+    return 'bg-track-c'
+  }
+})
+
+const trackContainerClass = computed(() => {
+  switch (props.trackIndex) {
+  case 0:
+    return props.isSponsorEvent
+      ? 'border-2 md:border-4 border-track-a text-track-a items-center justify-center text-center'
+      : 'bg-sponsor/[.03]'
+  case 1:
+    return props.isSponsorEvent
+      ? 'border-2 md:border-4 border-track-b text-track-b items-center justify-center text-center'
+      : 'bg-sponsor/[.03]'
+  case 2:
+    return props.isSponsorEvent
+      ? 'border-2 md:border-4 border-track-c text-track-c items-center justify-center text-center'
+      : 'bg-sponsor/[.03]'
+  default:
+    return ''
+  }
+})
+
+const trackName = computed(() => {
+  switch (props.trackIndex) {
+  case 0:
+    return 'メドピアトラック'
+  case 1:
+    return 'FUTUREトラック'
+  case 2:
+    return 'クラウドサイントラック'
+  }
+})
+</script>
+<template>
+  <div
+    class="flex flex-col md:pb-0"
+    :class="trackContainerClass"
+  >
+    <div
+      class="self-start p-1 min-w-[50%] text-xs text-center text-white md:hidden"
+      :class="trackLabelClasses"
+    >
+      {{ trackName }}
+    </div>
+    <div class="p-2">
+      <slot />
+    </div>
+  </div>
+</template>
