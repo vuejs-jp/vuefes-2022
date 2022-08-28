435e0407eb6cb7de74f15b3d79100288ca617ec5
James Roberts <robjam.github@gmail.com>
2022-08-29 02:44:48 +0900

    add speaker information

--- app/components/TimeTableSection.vue
+++ app/components/TimeTableSection.vue
@@ -139,13 +139,16 @@ const timeslots = [
     isSponsorEvent: false,
     events: [
       {
-        title: 'メインセッション',
+        title: '負債まみれのレガシーフロントエンド画面を Vue.js でリプレイスした話',
+        subTitle: 'とよへい'
       },
       {
-        title: 'メインセッション',
+        title: '不確実性のある将来に対応するためのデザイン戦略',
+        subTitle: '腹筋ローラーの力を信じろ'
       },
       {
-        title: 'メインセッション',
+        title: 'Vue2 Vue3 マイグレーション 令和最新 最強',
+        subTitle: 'miyaoka'
       },
     ],
   },
@@ -155,13 +158,22 @@ const timeslots = [
     isSponsorEvent: false,
     events: [
       {
-        title: 'メインセッション',
+        title: `1. レガシーなMPAアプリケーションをwebpackからviteに移行する話
+oreo
+2. provide/injectを用いたローカルな状態管理
+ebiryu
+3. Nuxt2 + Composition API から Nuxt Bridge へのマイグレーションのすゝめ
+掛水優輝
+4. Nuxt Content v2でエンジニアブログを作っている話
+古澤 棟熏`,
       },
       {
-        title: 'メインセッション',
+        title: 'Vue.js でアクセシブルなコンポーネントをつくるために',
+        subTitle: 'やまのく'
       },
       {
-        title: 'メインセッション',
+        title: 'こわくない」Vuetifyで始めるOSSコントリビュート',
+        subTitle: '川野邉 賢二'
       },
     ],
   },
@@ -202,7 +214,7 @@ const timeslots = [
             <TrackLabel
               v-for="(tracks, timeslotIndex) in timeslot.events"
               :key="timeslotIndex"
-              class="col-span-3 last:mb-2.5 md:col-span-1 md:last:mb-0"
+              class="col-span-3 last:mb-2.5 md:col-span-1 md:last:mb-0 whitespace-pre-wrap"
               :is-sponsor-event="timeslot.isSponsorEvent"
               :track-index="timeslotIndex"
             >
@@ -213,6 +225,29 @@ const timeslots = [
         <div
           class="grid grid-cols-1 gap-y-2.5 mx-auto mb-2.5 md:grid-cols-md-timetable md:gap-2.5 md:mb-0 xl:grid-cols-timetable xl:max-w-[1260px]"
         >
+          <p class="timeslot">
+            14:20 - 15:05
+          </p>
+          <TrackLabel
+            class="col-span-3 md:col-span-1 md:row-span-1"
+            :track-index="0"
+          >
+            <p class="text-base">14:20 - 14:40</p><h3 class="">施策を止めるな！Vue2からVue3への移行<br />志賀 奎太</h3><p class="mt-2.5 text-base">14:45 - 15:05</p><h3 class="">十数万レコードに耐えうるVue.jsプロジェクトを実現するためのパフォーマンスチューニング<br />tbashiyy</h3>
+          </TrackLabel>
+          <TrackLabel
+            class="col-span-3 justify-center md:col-span-1 md:row-span-1"
+            :track-index="1"
+          >
+            <h3 class="text-center">OSSはじめのいっぽ</h3>
+          </TrackLabel>
+
+          <TrackLabel
+            class="col-span-3 mb-2.5 md:col-span-1 md:mb-0"
+            :track-index="2"
+          >
+            <p class="text-base">14:20 - 14:40</p><h3 class="">JSからTSへ移行したVue.jsプロダクトの型チェックを漸進的に強化する<br />川俣 涼</h3><p class="mt-2.5 text-base">14:45 - 15:05</p><h3 class="">Guillaume Chau</h3>
+          </TrackLabel>
+
           <p class="timeslot">
             15:10 - 15:55
           </p>
@@ -226,32 +261,32 @@ const timeslots = [
             class="col-span-3 row-span-2 md:col-span-1 md:row-span-1"
             :track-index="1"
           >
-            <p>15:10 - 15:30</p><h3 class="">メインセッション</h3><p class="mt-2.5">15:10 - 15:30</p><h3 class="">メインセッション</h3>
+            <p class="text-base">15:10 - 15:30</p><h3 class="">Nuxt モジュールの作り方を知って 開発した機能を再利用しよう<br />田中弘治</h3><p class="mt-2.5 text-base">15:10 - 15:30</p><h3 class="">eslint-plugin-vueを使用して継続的にVue3移行する<br />太田 洋介</h3>
           </TrackLabel>
 
           <TrackLabel
             class="col-span-3 row-span-5 mb-2.5 md:col-span-1 md:mb-0"
             :track-index="2"
           >
-            <p>15:35 - 17:35</p><h3 class="">ハンズオン</h3>
+            <p class="text-base">15:10 - 15:30</p><h3 class="">Nuxt モジュールの作り方を知って 開発した機能を再利用しよう<br />田中弘治</h3>
+            <p class="mt-2.5 text-base">15:35 - 17:35</p><h3 class="mt-2.5 text-center">ハンズオン</h3>
           </TrackLabel>
 
           <p class="timeslot">
             16:00 - 16:20
           </p>
 
-
           <TrackLabel
             class="col-span-3 md:col-span-1"
             :track-index="0"
           >
-            <h3 class="">メインセッション</h3>
+            <h3 class="">安全に開発効率を上げるための Vue 2.7 移行<br />watsuyo</h3>
           </TrackLabel>
           <TrackLabel
             class="col-span-3 mb-2.5 md:col-span-1 md:mb-0"
             :track-index="1"
           >
-            <h3 class="">メインセッション</h3>
+            <h3 class="">Sebastien Chopin</h3>
           </TrackLabel>
 
           <p class="timeslot">
@@ -261,13 +296,13 @@ const timeslots = [
             class="col-span-3 md:col-span-1"
             :track-index="0"
           >
-            <h3 class="">メインセッション</h3>
+            <h3 class="">Matias Capeletto</h3>
           </TrackLabel>
           <TrackLabel
             class="col-span-3 mb-2.5 md:col-span-1 md:mb-0"
             :track-index="1"
           >
-            <h3 class="">メインセッション</h3>
+            <h3 class="">NuxtJSによるJamstack構築とNuxt 3でどう変わるのか<br/>菅家 大地</h3>
           </TrackLabel>
 
           <p class="timeslot">
@@ -277,13 +312,13 @@ const timeslots = [
             class="col-span-3 md:col-span-1"
             :track-index="0"
           >
-            <h3 class="">メインセッション</h3>
+            <h3 class="">Jessica Sachs</h3>
           </TrackLabel>
           <TrackLabel
             class="col-span-3 mb-2.5 md:col-span-1 md:mb-0"
             :track-index="1"
           >
-            <h3 class="">メインセッション</h3>
+            <h3 class="">デザインシステムを後から導入する前提で作った 変更に強いNuxt3プロジェクトの構成<br />みゅーとん</h3>
           </TrackLabel>
 
           <p class="timeslot">
@@ -293,13 +328,13 @@ const timeslots = [
             class="col-span-3 md:col-span-1"
             :track-index="0"
           >
-            <h3 class="">メインセッション</h3>
+            <h3 class="">社内用共通コンポーネントのビジュアルリグレッションテストにStorybookとChromaticを選択した話<br />プログラミングをするパンダ</h3>
           </TrackLabel>
           <TrackLabel
             class="col-span-3 mb-2.5 md:col-span-1 md:mb-0"
             :track-index="1"
           >
-            <h3 class="">メインセッション</h3>
+            <h3 class="">Anthony Fu</h3>
           </TrackLabel>
 
           <p class="timeslot">
