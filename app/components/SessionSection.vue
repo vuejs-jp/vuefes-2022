88c5ca14f33bd949c4e221b6594afc8fa988b54a
Kohei Tsuchiya <doyahirohira@gmail.com>
2022-09-01 22:49:04 +0900

    セッション詳細画面のマークアップを調整

--- app/components/SessionSection.vue
+++ app/components/SessionSection.vue
@@ -13,35 +13,35 @@ const props = defineProps({
 const speakerInfo = computed(() => props.speaker)
 </script>
 <template>
-  <section class="px-5 mx-auto max-w-[56.25rem] text-vue-blue">
+  <section class="px-5 mx-auto max-w-900 text-vue-blue">
     <span class="inline-block py-1 px-4 mb-2.5 text-sm font-extrabold text-white bg-vue-blue md:text-lg">
       {{ speakerInfo.session.time }}min
     </span>
-    <h3 class="mb-5 text-xl font-bold leading-8 md:text-[1.5625rem]">
+    <h2 class="mb-5 text-xl font-bold leading-8 md:mb-10 md:text-25">
       {{ speakerInfo.session.title }}
-    </h3>
+    </h2>
     <!-- eslint-disable vue/no-v-html -->
-    <p
-      class="mb-8 text-base leading-8 md:mb-20 md:text-lg md:leading-9"
+    <div
+      class="mb-8 text-sm leading-7 md:mb-20 md:text-lg md:leading-8 description"
       v-html="speakerInfo.session.description"
     />
     <!-- eslint-enaable vue/no-v-html -->
-    <div class="p-5 border border-vue-blue md:flex md:p-[3.125rem]">
-      <p class="shrink-0 mb-5 text-sm font-extrabold text-center md:mt-[3.75rem] md:mr-[3.125rem] md:text-[1.375rem]">
+    <div class="p-5 border border-vue-blue md:flex md:p-12.5">
+      <p class="shrink-0 mb-5 text-sm font-extrabold text-center md:mt-15 md:mr-12.5 md:text-22">
         Speaker Profile
       </p>
       <div>
         <div class="mb-5 md:flex md:mb-0">
           <img
-            class="mx-auto mb-5 w-28 h-28 md:mr-8 md:ml-0 md:w-[9.375rem] md:h-[9.375rem]"
+            class="mx-auto mb-5 w-28 h-28 md:mr-8 md:ml-0 md:w-37.5 md:h-37.5"
             :alt="`${speakerInfo.name}の写真`"
             :src="speakerInfo.image"
           >
           <div class="text-center md: md:pt-5 md:text-left">
-            <p class="text-[0.6875rem] opacity-60 md:text-sm">
+            <p class="text-11 opacity-60 md:text-sm">
               {{ speakerInfo.title }}
             </p>
-            <p class="mb-4 text-[0.9375rem] font-bold md:text-xl">
+            <p class="mb-4 text-15 font-bold md:text-xl">
               {{ speakerInfo.name }}
             </p>
             <span class="flex justify-center md:justify-start">
@@ -77,7 +77,7 @@ const speakerInfo = computed(() => props.speaker)
         </div>
         <!-- eslint-disable vue/no-v-html -->
         <p
-          class="text-sm leading-7 md:text-base md:leading-8"
+          class="text-xs leading-6 md:text-base md:leading-7"
           v-html="speakerInfo.description"
         />
         <!-- eslint-enaable vue/no-v-html -->
@@ -92,3 +92,11 @@ const speakerInfo = computed(() => props.speaker)
     </div>
   </section>
 </template>
+
+<style scoped>
+.description ::v-deep ul {
+  list-style: disc;
+  margin: revert;
+  padding: revert;
+}
+</style>
