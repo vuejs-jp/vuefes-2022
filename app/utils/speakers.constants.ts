f194743c92012d81b5ea04db0eb0f4268e27e92e
Kohei Tsuchiya <doyahirohira@gmail.com>
2022-09-05 21:29:08 +0900

    Jessさんのセッション情報を更新

--- app/utils/speakers.constants.ts
+++ app/utils/speakers.constants.ts
@@ -87,8 +87,9 @@ export const speakers = [
     twitter: '_JessicaSachs',
     github: 'JessicaSachs',
     session: {
-      title: '調整中',
-      description: '調整中',
+      title: 'Cypress 10 and Component Testing',
+      description:
+        '<p>An overview of Cypress 10\'s elegant re-designed UI and brand-new testing type: Component Testing! In this talk we will use TDD to build a modal using the new "Create from Component" feature. We will learn the benefits to using Cypress\'s driver and assertions over headless test runners.</p>',
       time: 20,
     },
   },
