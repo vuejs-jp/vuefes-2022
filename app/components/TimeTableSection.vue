e1f51bf4d29115d1d940e86cfb36a5aac5eebe39
James Roberts <robjam.github@gmail.com>
2022-09-13 20:15:02 +0900

    move constant data to utils

--- app/components/TimeTableSection.vue
+++ app/components/TimeTableSection.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import SectionTitle from '~/components/SectionTitle.vue'
-
 import TrackLabel from '~/components/timetable/TrackLabel.vue'
+import { timeslots } from '~/utils/timeslots.constants'
 function trackClassByIndex(index:number, isSponsorEvent: boolean) {
   switch (index) {
   case 0:
@@ -15,169 +15,6 @@ function trackClassByIndex(index:number, isSponsorEvent: boolean) {
     break
   }
 }
-const timeslots = [
-  {
-    time: '09:30 - 10:00',
-    isAllTrackEvent: true,
-    isSponsorEvent: false,
-    events: [
-      {
-        title: '会場',
-      },
-    ],
-  },
-  {
-    time: '10:00 - 10:10',
-    isAllTrackEvent: true,
-    isSponsorEvent: false,
-    events: [
-      {
-        title: 'オープニングムービー・挨拶｜Kazupon',
-      },
-    ],
-  },
-  {
-    time: '10:10 - 11:00',
-    isAllTrackEvent: true,
-    isSponsorEvent: false,
-    events: [
-      {
-        title: 'キーノート｜Evan You',
-      },
-    ],
-  },
-  {
-    time: '11:00 - 11:20',
-    isAllTrackEvent: false,
-    isSponsorEvent: true,
-    events: [
-      {
-        title: 'プラチナスポンサー#A',
-        subTitle: 'メドピア',
-      },
-      {
-        title: 'プラチナスポンサー#B',
-        subTitle: 'FUTURE',
-      },
-      {
-        title: 'プラチナスポンサー#C',
-        subTitle: 'クラウドサイン',
-      },
-    ],
-  },
-  {
-    time: '11:20 - 12:00',
-    isAllTrackEvent: true,
-    isSponsorEvent: false,
-    events: [
-      {
-        title: 'Evan Youに聞こう',
-      }
-    ],
-  },
-  {
-    time: '12:00 - 12:30',
-    isAllTrackEvent: true,
-    isSponsorEvent: false,
-    events: [
-      {
-        title: 'ランチタイム',
-      }
-    ],
-  },
-  {
-    time: '12:30 - 12:40',
-    isAllTrackEvent: false,
-    isSponsorEvent: true,
-    events: [
-      {
-        title: 'ゴールドスポンサー #A1',
-        subTitle: 'ヘイ株式会社',
-      },
-      {
-        title: 'ゴールドスポンサー #B1',
-        subTitle: 'LINE株式会社',
-      },
-      {
-        title: 'ゴールドスポンサー #C1',
-        subTitle: '株式会社イエソド',
-      },
-    ],
-  },
-  {
-    time: '12:45 - 12:55',
-    isAllTrackEvent: false,
-    isSponsorEvent: true,
-    events: [
-      {
-        title: 'ゴールドスポンサー #A2',
-        subTitle: 'グレープシティ株式会社',
-      },
-      {
-        title: 'ゴールドスポンサー #B2',
-        subTitle: '株式会社iCARE',
-      },
-      {
-        title: 'ゴールドスポンサー #C2',
-        subTitle: 'BASE株式会社',
-      },
-    ],
-  },
-  {
-    time: '13:00 - 13:30',
-    isAllTrackEvent: true,
-    isSponsorEvent: false,
-    events: [
-      {
-        title: 'ランチタイム',
-      }
-    ],
-  },
-  {
-    time: '13:30 - 13:50',
-    isAllTrackEvent: false,
-    isSponsorEvent: false,
-    events: [
-      {
-        title: '負債まみれのレガシーフロントエンド画面を Vue.js でリプレイスした話',
-        subTitle: 'とよへい'
-      },
-      {
-        title: '不確実性のある将来に対応するためのデザイン戦略',
-        subTitle: '腹筋ローラーの力を信じろ'
-      },
-      {
-        title: 'Vue2 Vue3 マイグレーション 令和最新 最強',
-        subTitle: 'miyaoka'
-      },
-    ],
-  },
-  {
-    time: '13:55 - 14:15',
-    isAllTrackEvent: false,
-    isSponsorEvent: false,
-    events: [
-      {
-        subTitle: `1. レガシーなMPAアプリケーションをwebpackからviteに移行する話
-oreo
-2. provide/injectを用いたローカルな状態管理
-ebiryu
-3. Nuxt2 + Composition API から Nuxt Bridge へのマイグレーションのすゝめ
-掛水優輝
-4. Nuxt Content v2でエンジニアブログを作っている話
-古澤 棟熏`,
-      },
-      {
-        title: 'Vue.js でアクセシブルなコンポーネントをつくるために',
-        subTitle: 'やまのく'
-      },
-      {
-        title: '「こわくない」Vuetifyで始めるOSSコントリビュート',
-        subTitle: '川野邉 賢二'
-      },
-    ],
-  },
-]
 </script>
 <template>
   <section
@@ -217,16 +54,11 @@ ebiryu
               class="speaker-track-label"
               :is-sponsor-event="timeslot.isSponsorEvent"
               :track-index="timeslotIndex"
-            >
-              <h3>
-                {{ tracks.title }}<p
+            ><h3>{{ tracks.title }}<p
                   class="text-sm leading-7 lg:text-lg lg:leading-8"
                   :class="{ 'font-normal': !timeslot.isSponsorEvent }"
-                >
-                  {{ tracks.subTitle }}
-                </p>
-              </h3>
-            </TrackLabel>
+                >{{ tracks.subTitle }}</p>
+              </h3></TrackLabel>
           </template>
         </div>
         <div
@@ -373,6 +205,7 @@ ebiryu
 
 .speaker-track-label {
   @apply col-span-3 last:mb-2.5 whitespace-pre-wrap md:col-span-1 md:last:mb-0
+
 
 }
 
