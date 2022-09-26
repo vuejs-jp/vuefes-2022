3084c2d452240a52bf183f7c5638526a55fd3ae6
Kohei Tsuchiya <doyahirohira@gmail.com>
2022-09-26 21:38:38 +0900

    ディナースポンサーを追加

--- app/utils/sponsors.constants.ts
+++ app/utils/sponsors.constants.ts
@@ -3,22 +3,24 @@ import { ISponsor, Rank } from '../types/sponsors'
 
 export const sponsorSupportLevelTitle = (rank: Rank) => {
   switch (rank) {
-    case 'platinum':
-      return 'Platinum'
-    case 'gold':
-      return 'Gold'
-    case 'silver':
-      return 'Silver'
-    case 'bronze':
-      return 'Bronze'
-    case 'media':
-      return 'Media'
-    case 'specialMedia':
-      return 'Special Media'
-    case 'streaming':
-      return 'Streaming'
-    default:
-      const _missing: never = rank
-      return ''
+  case 'platinum':
+    return 'Platinum'
+  case 'gold':
+    return 'Gold'
+  case 'silver':
+    return 'Silver'
+  case 'bronze':
+    return 'Bronze'
+  case 'media':
+    return 'Media'
+  case 'specialMedia':
+    return 'Special Media'
+  case 'streaming':
+    return 'Streaming'
+  case 'dinner':
+    return 'Dinner'
+  default:
+    const _missing: never = rank
+    return ''
   }
 }
